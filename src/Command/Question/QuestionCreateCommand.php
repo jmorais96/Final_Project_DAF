cf299a8ee87314a61b3c1e0cfc0521607d12bc6e
José Morais <jose.morais.96@hotmail.com>
2018-12-15 16:31:28 -0100

    Trying to fix doctrine problems

---
+++ src/Command/Question/QuestionCreateCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jose
+ * Date: 15-12-2018
+ * Time: 16:14
+ */
+
+namespace App\Command\Question;
+
+
+class QuestionCreateCommand
+{
+
+}
---
+++ src/Infrastructure/Persistence/Doctrine/Question/DoctrineQuestionRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jose
+ * Date: 15-12-2018
+ * Time: 15:38
+ */
+
+namespace App\Infrastructure\Persistence\Doctrine\Question;
+
+
+class DoctrineQuestionRepository
+{
+
+}
